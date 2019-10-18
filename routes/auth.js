c786395c798773b0a5cd4dba536a006c58795d23
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-18 15:30:49 -0700

    middleware.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const auth = require('../middleware.js');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const config = require('config')
--- routes/stories.js
+++ routes/stories.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const auth = require('../middleware/auth');
+const auth = require('../middleware.js');
 
 const Story = require('../models/Story');
 const User = require('../models/User');
