1759e408bf41e4166d7c4788d4833db53db11c0b
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-18 15:52:48 -0700

    redid setAuthToken

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,17 +1,11 @@
-const jwt = require('jsonwebtoken');
-const { SECRET } = process.env;
+import axios from 'axios';
 
-const createToken = user => jwt.sign({user, exp: Math.floor(Date.now() / 1000) + (60 * 60)}, SECRET);
-
-const validToken = token => {
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, SECRET, function (err, user) {
-      return err ? reject(err) : resolve(user);      
-    })
-  })
-} 
-
-module.exports = {
-  validToken, 
-  createToken
+const setAuthToken = token => {
+    if (token) {
+        axios.defaults.headers.common['x-auth-token'] = token;
+    } else {
+        delete axios.defaults.headers.common['x-auth-token'];
+    }
 }
+
+export default setAuthToken;
